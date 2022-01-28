4bf8af10f30dfc0613f7aa799f57f92217c9a1f4
pierfrancescoscar <scarinci401@gmail.com>
2022-01-28 14:52:41 +0100

    ADD: jokes array data

---
+++ config/joke-data.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+    [
+        "title" => "What’s the best thing about Switzerland?",
+        "description" => "I don’t know, but the flag is a big plus.",
+        "thumb" => "https://www.rd.com/wp-content/uploads/2018/06/01-Short-Jokes-Anyone-Can-Remember-nicole-fornabaio-rd.com_.jpg?resize=2048,1365",
+
+    ],
+
+    [
+        "title" => "I invented a new word!",
+        "description" => "Plagiarism!",
+        "thumb" => "https://www.rd.com/wp-content/uploads/2018/06/02-Short-Jokes-Anyone-Can-Remember-nicole-fornabaio-rd.com_.jpg?resize=2048,1365",
+
+    ],
+
+    [
+        "title" => "Did you hear about the mathematician who’s afraid of negative numbers?",
+        "description" => "He’ll stop at nothing to avoid them.",
+        "thumb" => "https://www.rd.com/wp-content/uploads/2018/06/03-Short-Jokes-Anyone-Can-Remember-nicole-fornabaio-rd.com_.jpg?resize=2048,1365",
+
+    ],
+
+    [
+        "title" => "Why do we tell actors to “break a leg?",
+        "description" => "Because every play has a cast.",
+        "thumb" => "https://www.rd.com/wp-content/uploads/2018/06/04-Short-Jokes-Anyone-Can-Remember-nicole-fornabaio-rd.com_.jpg?resize=2048,1365",
+
+    ],
+
+    [
+        "title" => "Why do we tell actors to “break a leg?",
+        "description" => "Because every play has a cast.",
+        "thumb" => "https://www.rd.com/wp-content/uploads/2018/06/04-Short-Jokes-Anyone-Can-Remember-nicole-fornabaio-rd.com_.jpg?resize=2048,1365",
+
+    ],
+
+    [
+        "title" => "Helvetica and Times New Roman walk into a bar.",
+        "description" => "Get out of here!” shouts the bartender. “We don’t serve your type.",
+        "thumb" => "https://www.rd.com/wp-content/uploads/2018/06/05-Short-Jokes-Anyone-Can-Remember-nicole-fornabaio-rd.com_.jpg?resize=2048,1365",
+
+    ],
+
+    [
+        "title" => "Yesterday I saw a guy spill all his Scrabble letters on the road. I asked him, “What’s the word on the street?",
+        "description" => "Once my dog ate all the Scrabble tiles. For days he kept leaving little messages around the house.",
+        "thumb" => "https://www.rd.com/wp-content/uploads/2018/06/06-Short-Jokes-Anyone-Can-Remember-nicole-fornabaio-rd.com_.jpg?resize=2048,1365",
+
+    ],
+    
+];
