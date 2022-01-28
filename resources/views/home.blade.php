a2948b6dce05fa66d4f71caf3beffd75e5ba9279
pierfrancescoscar <scarinci401@gmail.com>
2022-01-28 15:01:52 +0100

    ADD: some content to homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,13 @@
 <section class="container-fluid">
     <div class="container p-5">
             <div class="row">
-                <div class="col-12">
+                <div class="col-12 d-flex justify-content-center flex-column">
                     <h3 class="mb-5 text-center">Joke List</h3>
                 </div>
+            </div>
+
+            <div class="col-12 d-flex justify-content-center">
+                <button class="btn btn-primary">Read all jokes</button>
             </div>
     </div>
 </section>
