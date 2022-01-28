56d0c67c9787f82984df5c3c6208006d0189334f
pierfrancescoscar <scarinci401@gmail.com>
2022-01-28 15:17:51 +0100

    ADD: jokes seeder w/conf of joke-data

---
+++ database/seeds/JokesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Joke;
+
+
+class JokesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $jokes = config('joke-data');
+
+        foreach ($jokes as $joke) {
+
+            $new_comic = new Joke();
+
+            $new_joke->title = $joke['title'];
+            $new_joke->description = $joke['description'];
+            $new_joke->thumb = $joke['thumb'];
+
+            $new_joke->save();
+        }
+    }
+}
